b619424f64abe28fcb2df3a74e8b87b038914ced
automaton82 <terrence.sheflin@gmail.com>
2023-10-13 10:38:15 -0400

    Adding optional hooks for session continuity.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -146,6 +146,12 @@ export class App {
             })
         }
 
+        // Logi Symphony authorization check.
+        if (process.env.LOGI_SYMPHONY_URL) {
+            const logiSymphony = await import('./utils/LogiSymphony/logisymphony')
+            logiSymphony.setupRequestAuthorization(this.app)
+        }
+
         const upload = multer({ dest: `${path.join(__dirname, '..', 'uploads')}/` })
 
         // ----------------------------------------
@@ -1185,6 +1191,12 @@ export class App {
         try {
             const chatflowid = req.params.id
             let incomingInput: IncomingInput = req.body
+
+            // Logi Symphony session ID override config injection check.
+            if (process.env.LOGI_SYMPHONY_URL) {
+                const logiSymphony = await import('./utils/LogiSymphony/logisymphony')
+                logiSymphony.checkSessionIdOverrideConfig(req, incomingInput)
+            }
 
             let nodeToExecuteData: INodeData
 
