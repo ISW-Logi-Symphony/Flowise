64bd2ff447d8d65cc15d3db220f812adc2264242
automaton82 <terrence.sheflin@gmail.com>
2023-11-07 17:16:45 -0500

    Revert "Revert "Squashed commit of the following:""
    This reverts commit a886bd14e11527ae4f729598e1fb8afaaf8f051f.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -9,6 +9,7 @@ import { Server } from 'socket.io'
 import logger from './utils/logger'
 import { expressRequestLogger } from './utils/logger'
 import { v4 as uuidv4 } from 'uuid'
+import OpenAI from 'openai'
 import { Between, IsNull, FindOptionsWhere } from 'typeorm'
 import {
     IChatFlow,
@@ -53,6 +54,7 @@ import { ChatFlow } from './database/entities/ChatFlow'
 import { ChatMessage } from './database/entities/ChatMessage'
 import { Credential } from './database/entities/Credential'
 import { Tool } from './database/entities/Tool'
+import { Assistant } from './database/entities/Assistant'
 import { ChatflowPool } from './ChatflowPool'
 import { CachePool } from './CachePool'
 import { ICommonObject, IMessage, INodeOptionsValue, handleEscapeCharacters } from 'flowise-components'
@@ -711,13 +713,13 @@ export class App {
         // ----------------------------------------
 
         // Get all assistants
-        this.app.get('/api/v1/assistants', async (req: Request, res: Response) => {
+        indexRouter.get('/api/v1/assistants', async (req: Request, res: Response) => {
             const assistants = await this.AppDataSource.getRepository(Assistant).find()
             return res.json(assistants)
         })
 
         // Get specific assistant
-        this.app.get('/api/v1/assistants/:id', async (req: Request, res: Response) => {
+        indexRouter.get('/api/v1/assistants/:id', async (req: Request, res: Response) => {
             const assistant = await this.AppDataSource.getRepository(Assistant).findOneBy({
                 id: req.params.id
             })
@@ -725,7 +727,7 @@ export class App {
         })
 
         // Get assistant object
-        this.app.get('/api/v1/openai-assistants/:id', async (req: Request, res: Response) => {
+        indexRouter.get('/api/v1/openai-assistants/:id', async (req: Request, res: Response) => {
             const credentialId = req.query.credential as string
             const credential = await this.AppDataSource.getRepository(Credential).findOneBy({
                 id: credentialId
@@ -751,7 +753,7 @@ export class App {
         })
 
         // List available assistants
-        this.app.get('/api/v1/openai-assistants', async (req: Request, res: Response) => {
+        indexRouter.get('/api/v1/openai-assistants', async (req: Request, res: Response) => {
             const credentialId = req.query.credential as string
             const credential = await this.AppDataSource.getRepository(Credential).findOneBy({
                 id: credentialId
@@ -771,7 +773,7 @@ export class App {
         })
 
         // Add assistant
-        this.app.post('/api/v1/assistants', async (req: Request, res: Response) => {
+        indexRouter.post('/api/v1/assistants', async (req: Request, res: Response) => {
             const body = req.body
 
             if (!body.details) return res.status(500).send(`Invalid request body`)
@@ -886,7 +888,7 @@ export class App {
         })
 
         // Update assistant
-        this.app.put('/api/v1/assistants/:id', async (req: Request, res: Response) => {
+        indexRouter.put('/api/v1/assistants/:id', async (req: Request, res: Response) => {
             const assistant = await this.AppDataSource.getRepository(Assistant).findOneBy({
                 id: req.params.id
             })
@@ -994,7 +996,7 @@ export class App {
         })
 
         // Delete assistant
-        this.app.delete('/api/v1/assistants/:id', async (req: Request, res: Response) => {
+        indexRouter.delete('/api/v1/assistants/:id', async (req: Request, res: Response) => {
             const assistant = await this.AppDataSource.getRepository(Assistant).findOneBy({
                 id: req.params.id
             })
@@ -1749,10 +1751,16 @@ export class App {
                       logger,
                       appDataSource: this.AppDataSource,
                       databaseEntities,
-                      analytic: chatflow.analytic
+                      analytic: chatflow.analytic,
+                      chatId
                   })
 
             result = typeof result === 'string' ? { text: result } : result
+
+            // Retrieve threadId from assistant if exists
+            if (typeof result === 'object' && result.assistant) {
+                sessionId = result.assistant.threadId
+            }
 
             const userMessage: Omit<IChatMessage, 'id'> = {
                 role: 'userMessage',
